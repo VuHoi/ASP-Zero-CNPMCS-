fdef8c33867c225348f644d9402d1f1ff268844d
hoideptrai123 <toilahoi007@gmail.com>
2019-04-06 12:30:48 +0700

    add dto

---
+++ src/GWebsite.AbpZeroTemplate.Application.Share/Product/Dto/ProductDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GWebsite.AbpZeroTemplate.Application.Share.Product.Dto
+{
+    public partial class ProductDto
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public double Discount { get; set; }
+        public string Summary { get; set; }
+        public string AvailableAddress { get; set; }
+        public DateTime AddedDate { get; set; }
+        public ImageDto ImageDto { get; set; }
+        public SupplierDto SupplierDto { get; set; }
+    }
+}
---
+++ src/GWebsite.AbpZeroTemplate.Application.Share/Product/IProductAppService.cs
@@ -0,0 +1,22 @@
+﻿using Abp.Application.Services.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GWebsite.AbpZeroTemplate.Application.Share.Product
+{
+    public interface IProductAppService
+    {
+        Task<ListResultDto<Product>> GetMenuClientsAsync();
+
+
+
+        Task<MenuClientDto> CreateMenuClientAsync(CreateMenuClientInput input);
+
+        Task<MenuClientDto> UpdateMenuClientAsync(UpdateMenuClientInput input);
+
+        Task DeleteMenuClientAsync(EntityDto<int> input);
+    }
+}
